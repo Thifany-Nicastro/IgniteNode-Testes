7f63b9948d99ebb3a7e2504775e0e8066b32a996
Thifany Nicastro <thifanynicastro@hotmail.com>
2021-04-07 10:05:39 -0300

    TDD na prática

--- src/modules/cars/dtos/ICreateCarDTO.ts
+++ src/modules/cars/dtos/ICreateCarDTO.ts
@@ -1,23 +1,11 @@
-class Car {
-  id: string;
-
+interface ICreateCarDTO {
   name: string;
-
   description: string;
-
   daily_rate: number;
-
-  available: boolean;
-
   license_plate: string;
-
   fine_amount: number;
-
   brand: string;
-
   category_id: string;
-
-  created_at: Date;
 }
 
-export { Car };
+export { ICreateCarDTO };
